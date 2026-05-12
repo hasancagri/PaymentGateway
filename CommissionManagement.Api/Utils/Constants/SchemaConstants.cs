399ad4d516808e499e73895608d68cfa939ddb80
hasancagri <hasancagridemiriz@gmail.com>
2026-05-12 11:09:45 +0300

    refactor: add SchemaConstants to CommissionManagement.Api

---
+++ CommissionManagement.Api/Utils/Constants/SchemaConstants.cs
@@ -0,0 +1,6 @@
+namespace CommissionManagement.Api.Utils.Constants;
+
+public class SchemaConstants
+{
+    public static readonly string COMMISSION_MANAGEMENT_SCHEMA_NAME = "commissionManagement";
+}
