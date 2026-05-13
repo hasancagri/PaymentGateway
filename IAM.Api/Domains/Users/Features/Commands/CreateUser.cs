0f15d8188eadbc6770333b91ebf17a333a47a276
hasancagri <hasancagridemiriz@gmail.com>
2026-05-13 07:38:48 +0300

    feat: CreateUser calls Keycloak Admin API before writing UserProfile to Marten

--- IAM.Api/Domains/Users/Features/Commands/CreateUser.cs
+++ IAM.Api/Domains/Users/Features/Commands/CreateUser.cs
@@ -21,25 +21,44 @@ public static class CreateUser
         public async Task<FeatureObjectResultModel<CreateUserResponse>> Handle(
             CreateUserCommand cmd,
             IDocumentSession session,
+            IAM.Api.Keycloak.KeycloakAdminClient keycloak,
             CancellationToken ct)
         {
-            var emailExists = await session.Query<User>().AnyAsync(x => x.Email.Value == cmd.Email, ct);
+            var emailExists = await session.Query<User>()
+                .AnyAsync(x => x.Email.Value == cmd.Email, ct);
+
             if (emailExists)
                 return FeatureObjectResultModel<CreateUserResponse>.Error(new MessageItem
                 {
                     Table = nameof(User),
-                    Code = CommonResourceConstants.COMMON_MESSAGE_RECORD_DUPLICATE
+                    Code  = CommonResourceConstants.COMMON_MESSAGE_RECORD_DUPLICATE
                 });
 
-            var userResult = User.Create(cmd.Email, cmd.Password, cmd.FirstName, cmd.LastName);
+            Guid keycloakId;
+            try
+            {
+                keycloakId = await keycloak.CreateUserAsync(
+                    cmd.Email, cmd.Password, cmd.FirstName, cmd.LastName, ct);
+            }
+            catch (Exception)
+            {
+                return FeatureObjectResultModel<CreateUserResponse>.Error(new MessageItem
+                {
+                    Table = nameof(User),
+                    Code  = "User.KeycloakCreateFailed"
+                });
+            }
+
+            var userResult = User.Create(keycloakId, cmd.Email, cmd.FirstName, cmd.LastName);
             if (!userResult.IsSuccess)
+            {
+                await keycloak.DeleteUserAsync(keycloakId, ct);
                 return FeatureObjectResultModel<CreateUserResponse>.Error(userResult.Messages!);
+            }
 
             session.Store(userResult.Data!);
-            return FeatureObjectResultModel<CreateUserResponse>.Ok(new CreateUserResponse
-            {
-                Id = userResult.Data!.Id
-            });
+            return FeatureObjectResultModel<CreateUserResponse>.Ok(
+                new CreateUserResponse { Id = userResult.Data!.Id });
         }
     }
 }
